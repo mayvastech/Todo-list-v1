174f34017bbb29b0413b557dd3c54f5029a0f7ad
mayvastech <65177211+mayvastech@users.noreply.github.com>
2023-07-24 01:12:48 +0100

    styled addTaskBtn + grabbed html dom elements

---
+++ app.js
@@ -0,0 +1,4 @@
+// Grabbing html elements
+const todoInput = document.getElementById("todo-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
