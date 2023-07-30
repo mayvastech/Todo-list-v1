7ffd4aa04dec7d10ee79a5ba24f8e8eb47cd46f3
mayvastech <65177211+mayvastech@users.noreply.github.com>
2023-07-30 16:42:21 +0100

    js functionality- add task, create new task items

--- app.js
+++ app.js
@@ -2,3 +2,25 @@
 const todoInput = document.getElementById("todo-input");
 const addTaskBtn = document.getElementById("add-task-btn");
 const todoList = document.getElementById("todo-list");
+
+// Add a task
+const addTask = function () {
+  const taskText = todoInput.ariaValueMax.trim();
+  if (taskText !== "") {
+    const taskItem = createTaskItem(taskText);
+  }
+};
+// Create new task items
+const createTaskItem = function (taskText) {
+  const taskItem = document.createElement("li");
+  taskItem.className = "todo-item";
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.classList.add("checkbox");
+  
+};
+// Delete tasks
+// Cross out tasks
+// Event listeners
+// Examples of pre-loaded tasks
